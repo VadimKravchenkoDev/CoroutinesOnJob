c4f73fb803055b0dd0a254ea3048028e40681b6f
VadimKravchenkoDev <arhizmei@gmail.com>
2023-12-25 17:57:34 +0200

    it work!

--- app/src/main/java/com/kravchenkovadim/coroutineonjob/main.kt
+++ app/src/main/java/com/kravchenkovadim/coroutineonjob/main.kt
@@ -2,7 +2,6 @@ package com.kravchenkovadim.coroutineonjob
 
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
-import kotlin.random.Random
 import java.util.Random
 import kotlinx.coroutines.*
 
@@ -14,6 +13,6 @@ fun main():Unit = runBlocking {
 }
 
 suspend fun doWork(name: String): String {
-    delay(Random().nextInt(5000).toLong())
+    delay(Random(100).nextInt(5000).toLong())
     return "Done. $name"
 }
